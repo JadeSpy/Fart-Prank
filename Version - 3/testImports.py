7965cfc8912f0cca77daf4323959806450d42c42
Jasper <58379062+JadeSpy@users.noreply.github.com>
2020-09-01 18:09:08 -0700

    Add files via upload

---
+++ Version - 3/testImports.py
@@ -0,0 +1,23 @@
+try:
+	from playsound import playsound
+	import random
+	import win32gui
+	import win32api
+	import socket
+
+	import numpy as np
+	import struct ## new
+	import inspect
+
+	import sys
+	import cv2
+	import io
+	import socket
+	import struct
+	import time
+	import pickle
+	import zlib
+	import threading
+except Exception as e:
+	print(e)
+	input()
